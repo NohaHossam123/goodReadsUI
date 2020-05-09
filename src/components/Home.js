59d26c053a603d776faa116d9f2c353ac3984c48
mena996 <menaemad996@gmail.com>
2020-05-09 23:00:56 +0200

    adding home ui v1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,78 @@
-
-
+import React from 'react';
 const Home = () => {
-    return ( 
-        "Home"
-     );
+    return (
+        <div className="container col-12">
+            <div className="card border-dark mb-3">
+                <div className="card-header">Welcome To Good Reads</div>
+            </div>
+            <div className="row m-1">
+                <div className="card-deck col-6">
+                    <div className="row">
+                        <div className="card border-dark mb-3 col-6">
+                            <div className="card-header">Header</div>
+                            <div className="card-body text-dark">
+                                <h5 className="card-title">Dark card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                        <div className="card border-dark mb-3 col-6">
+                            <div className="card-header">Header</div>
+                            <div className="card-body text-dark">
+                                <h5 className="card-title">Dark card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="card border-dark mb-3 col-7">
+                            <div className="card-header">Header</div>
+                            <div className="card-body text-dark">
+                                <h5 className="card-title">Dark card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col ">
+                    <div className="card border-dark mb-3">
+                        <div className="container">
+                            <div className="form-group">
+                                <h2>New here? Create a free account</h2>
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputEmail1">first name</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="first name" />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputEmail1">last name</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="last name" />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address" />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputEmail1">password</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="password" />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputEmail1">Retype Password</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Retype Password" />
+                            </div>
+                            <div className="custom-file mb-3">
+                                <input type="file" className="custom-file-input" id="validatedCustomFile" required />
+                                <label className="custom-file-label" for="validatedCustomFile">upload your image</label>
+                                <div className="invalid-feedback">Example invalid custom file feedback</div>
+                            </div>
+                            <div className="form-group mb-3 text-center">
+                            <button class="btn btn-primary col-2" type="submit">Signup</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Home;
