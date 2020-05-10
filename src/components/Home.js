b08aac2e3b33cca9f5e63e140e802acfedb64f84
mena996 <menaemad996@gmail.com>
2020-05-10 21:46:41 +0200

    fix ui in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,24 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="container col-12">
-            <div className="card border-dark mb-3">
-                <div className="card-header">Welcome To Good Reads</div>
+            <div className="card border-dark mb-3 col-12">
+                <div className="card-header col-12">
+                <form className="form-inline" action="/action_page.php">
+                        <div className="form-group col-6">
+                            <label for="email">Welcome To Good Reads</label>
+                        </div>
+                        <div className="form-group col-2">
+                            <input type="email" placeholder="Username" className="form-control  col-12" id="email" />
+                        </div>
+                        <div className="form-group col-2">
+                            <input type="password" placeholder="Password" className="form-control col-12" id="pwd" />
+                        </div>
+                        <div className="checkbox col-1">
+                            <label><input type="checkbox" /> Remember me </label>
+                        </div>
+                        <button className="btn btn-primary col-1" type="submit">login</button>
+                    </form>
+                </div>
             </div>
             <div className="row m-1">
                 <div className="card-deck col-6">
@@ -65,12 +81,36 @@ const Home = () => {
                                 <div className="invalid-feedback">Example invalid custom file feedback</div>
                             </div>
                             <div className="form-group mb-3 text-center">
-                            <button class="btn btn-primary col-2" type="submit">Signup</button>
+                                <button className="btn btn-primary col-2" type="submit">Signup</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav container col-12">
+                        <li className="nav-item active col-2 ml-5">
+                            <a className="nav-link " href="#"><h1> Home </h1><span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item col-2">
+                            <a className="nav-link" href="#"><h1> About us</h1></a>
+                        </li>
+                        <li className="nav-item col-2">
+                            <a className="nav-link" href="#"><h1> Categories</h1></a>
+                        </li>
+                        <li className="nav-item col-2">
+                            <a className="nav-link" href="#"><h1> Authors</h1></a>
+                        </li>
+                        <li className="nav-item col-3">
+                            <a className="nav-link" href="#"><h1> Terms & Conditions</h1></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 }
